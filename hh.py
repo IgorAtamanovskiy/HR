0848657c29fb07ede215af14d34e310c883d35ec
IgorAtamanovskiy <36031292+IgorAtamanovskiy@users.noreply.github.com>
2019-09-12 18:07:59 +0300

    HH

--- hh.py
+++ hh.py
@@ -2,74 +2,128 @@
 import requests
 import pandas as pd
 import datetime
+import json
 
-def GetPositionSalaryEstimate(JobTitle,currency):
+
+def GetDataFromSupplier(JobTitle):
     number_of_pages = 10
-    job_title = [JobTitle]# ["'Frontend developer' and 'Angular' and 'Senior'"]
-    for job in job_title:
-        data=[]
-        for i in range(number_of_pages):
-            url = 'https://api.hh.ru/vacancies'
-            par = {'text': job, 'area':'1','per_page':'10', 'page':i}
-            r = requests.get(url, params=par)
-            e=r.json()
-            data.append(e)
-            vacancy_details = data[0]['items'][0].keys()
-            df = pd.DataFrame(columns=list(vacancy_details))
-            ind = 0
-            for i in range(len(data)):
-                for j in range(len(data[i]['items'])):
-                    df.loc[ind] = data[i]['items'][j]
-                    ind+=1
+    job = JobTitle
+    data = []
+    for i in range(number_of_pages):
+        url = "https://api.hh.ru/vacancies"
+        par = {"text": job, "area": "1", "per_page": "10", "page": i}
+        r = requests.get(url, params=par)
+        e = r.json()
+        data.append(e)
 
-        salaries = df.salary.dropna()
-        VacDescription=df.snippet
-        #print(VacDescription)
+    filename = job
+    filename = filename.replace(" AND ", "-").replace("'", "")
+    filename = "responses\hh-" + filename + ".json"
+    outfile = open(filename, "w+")
+    json.dump(data, outfile)
+    outfile.close()
 
-        frm=[x['from'] for x in salaries]
-        to = [x['to'] for x in salaries]
-        cur=[x['currency'] for x in salaries]
+    return data
 
-        descr=[x['requirement'] for x in VacDescription]
-        VD={'Description':descr}
-        VD_DF=pd.DataFrame(VD)
-        print(VD_DF)
 
-        d={'from':frm,'to':to,'currency':cur}
-        ddf=pd.DataFrame(d)
+def GetPositionSalaryEstimate(JobTitle, currency):
 
-        currency_filter=ddf['currency']==currency
-        ddfc=ddf[currency_filter]
+    JsonData = GetDataFromSupplier(JobTitle)
 
-        #wordcloud
+    vacancy_details = JsonData[0]["items"][0].keys()
+    df = pd.DataFrame(columns=list(vacancy_details))
+    ind = 0
+    for i in range(len(JsonData)):
+        for j in range(len(JsonData[i]["items"])):
+            df.loc[ind] = JsonData[i]["items"][j]
+            ind += 1
 
-        from collections import Counter
-        vacancy_names = df.name
-        cloud = Counter(vacancy_names)
-        #print(cloud)
-        from wordcloud import WordCloud, STOPWORDS
-        stopwords = set(STOPWORDS)
-        cloud = ''
-        for x in list(vacancy_names):
-            cloud += x + ' '
-        wordcloud = WordCloud(width=800, height=800,
+    print(df)
 
-                              stopwords=stopwords,
-                              min_font_size=8, background_color='white'
-                              ).generate(cloud)
+    salaries = df.salary
+    df_names = df.name
 
-        import matplotlib.pylab as plt
-        plt.figure(figsize=(16, 16))
-        plt.axis("off")
-        plt.imshow(wordcloud)
-        imgpath="imgs\\" + repr(datetime.datetime.now().timestamp())+'_vacancy_cloud.png'
-        plt.savefig(imgpath)
+    print(df_names)
 
-        priceestimate = [ddfc['from'].min(), ddfc['to'].max(), ddfc['to'].median(), round(ddfc['to'].mean(), 1),imgpath]
+
+    VacDescription = df.snippet
+
+    dfJoin = pd.concat([df_names, salaries], axis=1)
+
+    dfJoinWONA = dfJoin.salary.dropna()
+
+    print(dfJoinWONA)
+
+    # print(salaries.head())
+
+    frm = [x["from"] for x in salaries]
+    to = [x["to"] for x in salaries]
+    cur = [x["currency"] for x in salaries]
+
+    descr = [x["requirement"] for x in VacDescription]
+
+    VD = {"Description": descr}
+    VD_DF = pd.DataFrame(VD)
+
+    d = {"from": frm, "to": to, "currency": cur}
+    ddf = pd.DataFrame(d)
+
+    dfVacNameWPrice = pd.concat([df.name, ddf], axis=1)
+
+    print(dfVacNameWPrice)
+
+    currency_filter = ddf["currency"] == currency
+    ddfc = ddf[currency_filter]
+
+    # wordcloud
+
+    from collections import Counter
+
+    vacancy_names = df.name
+    cloud = Counter(vacancy_names)
+    # print(cloud)
+    from wordcloud import WordCloud, STOPWORDS
+
+    stopwords = set(STOPWORDS)
+    cloud = ""
+    for x in list(vacancy_names):
+        cloud += x + " "
+
+    wordcloud = WordCloud(
+        width=800,
+        height=800,
+        stopwords=stopwords,
+        min_font_size=8,
+        background_color="black",
+    ).generate(cloud)
+
+    import matplotlib.pylab as plt
+
+    plt.figure(figsize=(16, 16))
+    plt.axis("off")
+    plt.imshow(wordcloud)
+    imgpath = (
+        "imgs\\" + repr(datetime.datetime.now().timestamp()) + "_vacancy_cloud.png"
+    )
+    plt.savefig(imgpath)
+
+    print(ddfc)
+
+    priceestimate = [
+        ddfc["from"].min(),
+        ddfc["to"].max(),
+        ddfc["to"].median(),
+        round(ddfc["to"].mean(), 1),
+        imgpath,
+        ddfc,
+    ]
 
     return priceestimate
 
-        #print(repr(ddfc['from'].min()) + ' ' + repr(ddfc['to'].max()) + ' ' + repr(ddfc['to'].median()) + ' ' + repr(ddfc['to'].mean()))
 
-if __name__ == '__main__':
-    price=GetPositionSalaryEstimate("'Frontend developer' and 'React'",'RUR')
+if __name__ == "__main__":
+    GetPositionSalaryEstimate("'angular' AND 'Frontend Developer'" , "RUB")
+
+    #print(range(len(JsonData)))
+
+    # price=GetPositionSalaryEstimate("'секретарь'",'RUR')
